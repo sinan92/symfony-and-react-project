cd6ce3cf8a920896d171a4aa5cea11a663bd9494
QuanDar <fluxwt@gmail.com>
2018-09-25 12:43:41 +0200

    phpunit testen searchByContent written
    phpunit testen searchByContent written

--- src/Model/PDOMessageModel.php
+++ src/Model/PDOMessageModel.php
@@ -54,7 +54,7 @@ class PDOMessageModel implements IMessageModel
         $statement->bindColumn(1, $id, \PDO::PARAM_INT);
         $statement->bindColumn(2, $content, \PDO::PARAM_STR);
         $statement->bindColumn(3, $category, \PDO::PARAM_STR);
-        $statement->bindColumn(4, $date, \PDO::PARAM_INT);
+        $statement->bindColumn(4, $date, \PDO::PARAM_STR);
         $statement->bindColumn(5, $upVotes, \PDO::PARAM_INT);
         $statement->bindColumn(6, $downVotes, \PDO::PARAM_INT);
 
@@ -65,16 +65,35 @@ class PDOMessageModel implements IMessageModel
         return $messages;
     }
 
-    public function searchByContentAndCategory($search)
+    public function searchByCategory($category)
     {
-        $statement = $this->connection->getPDO()->prepare("SELECT * FROM messages WHERE content LIKE ? OR  category LIKE ?");
-        $statement->bindValue(1, "%" . $search . "%", \PDO::PARAM_STR);
-        $statement->bindValue(2, "%" . $search . "%", \PDO::PARAM_STR);
+        $statement = $this->connection->getPDO()->prepare("SELECT * FROM messages WHERE category LIKE ?");
+        $statement->bindValue(1, "%" . $category . "%", \PDO::PARAM_STR);
         $statement->execute();
         $statement->bindColumn(1, $id, \PDO::PARAM_INT);
         $statement->bindColumn(2, $content, \PDO::PARAM_STR);
         $statement->bindColumn(3, $category, \PDO::PARAM_STR);
-        $statement->bindColumn(4, $date, \PDO::PARAM_INT);
+        $statement->bindColumn(4, $date, \PDO::PARAM_STR);
+        $statement->bindColumn(5, $upVotes, \PDO::PARAM_INT);
+        $statement->bindColumn(6, $downVotes, \PDO::PARAM_INT);
+
+        $messages = [];
+        while($statement->fetch(\PDO::FETCH_BOUND)){
+            $messages[] = ['id' => $id, 'content' => $content, 'category' => $category, 'date' => $date, 'upVotes' => $upVotes, 'downVotes' => $downVotes];
+        }
+        return $messages;
+    }
+
+    public function searchByContentAndCategory($content, $category)
+    {
+        $statement = $this->connection->getPDO()->prepare("SELECT * FROM messages WHERE content LIKE ? AND category LIKE ?");
+        $statement->bindValue(1, "%" . $content . "%", \PDO::PARAM_STR);
+        $statement->bindValue(2, "%" . $category . "%", \PDO::PARAM_STR);
+        $statement->execute();
+        $statement->bindColumn(1, $id, \PDO::PARAM_INT);
+        $statement->bindColumn(2, $content, \PDO::PARAM_STR);
+        $statement->bindColumn(3, $category, \PDO::PARAM_STR);
+        $statement->bindColumn(4, $date, \PDO::PARAM_STR);
         $statement->bindColumn(5, $upVotes, \PDO::PARAM_INT);
         $statement->bindColumn(6, $downVotes, \PDO::PARAM_INT);
 
@@ -115,6 +134,18 @@ class PDOMessageModel implements IMessageModel
         $statement->execute();
 
         return $statement->rowCount() > 0;
+    }
+
+    public function idExists($id)
+    {
+        $this->validateId($id);
+        $statement = $this->pdo->prepare('SELECT id from messages WHERE id=:id');
+        $statement->bindParam(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        if ($statement->fetch() === FALSE) {
+            return FALSE;
+        }
+        return TRUE;
     }
 
     private function generateToken(){
--- tests/Model/PDOMessageModelTest.php
+++ tests/Model/PDOMessageModelTest.php
@@ -1,8 +1,8 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Model\PDOMessageModel;
-use Model\Connection;
+use App\Model\PDOMessageModel;
+use App\Model\Connection;
 
 class PDOMessageModelTest extends TestCase {
     public function setUp()
@@ -45,29 +45,30 @@ class PDOMessageModelTest extends TestCase {
     public function providerMessages()
     {
         return [
-            ['id' => 1, 'content' => 'Test', 'category' => 'Comedy', 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000)), 'upVotes' => 10, 'downVotes' => 30],
-            ['id' => 2, 'content' => 'Test2', 'category' => 'Horror', 'date' => date("Y-m-d H:i:s", mktime(0, 0, 0, 2, 1, 2000)), 'upVotes' => 20, 'downVotes' => 20],
-            ['id' => 3, 'content' => 'Test3', 'category' => 'Thriller', 'date' => date("Y-m-d H:i:s", mktime(0, 0, 0, 10, 30, 2010)), 'upVotes' => 30, 'downVotes' => 10]
+            ['id' => 1, 'content' => 'Content1', 'category' => 'Comedy', 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000)), 'upVotes' => 10, 'downVotes' => 30],
+            ['id' => 2, 'content' => 'Content2', 'category' => 'Horror', 'date' => date("Y-m-d H:i:s", mktime(0, 0, 0, 2, 1, 2000)), 'upVotes' => 20, 'downVotes' => 20],
+            ['id' => 3, 'content' => 'Content3', 'category' => 'Thriller', 'date' => date("Y-m-d H:i:s", mktime(0, 0, 0, 10, 30, 2010)), 'upVotes' => 30, 'downVotes' => 10]
         ];
     }
 
     public function providerComments()
     {
-        return [['id'=> 1,'message_id'=> 1, 'content' => "Content1", 'token' => 10001001, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 2,'message_id'=> 1, 'content' => "Content2", 'token' => 10001011, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 3,'message_id'=> 1, 'content' => "Content3", 'token' => 10001100, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 4,'message_id'=> 1, 'content' => "Content4", 'token' => 10001010, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 5,'message_id'=> 2, 'content' => "Content5", 'token' => 10001000, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 6,'message_id'=> 2, 'content' => "Content6", 'token' => 10101000, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 7,'message_id'=> 2, 'content' => "Content7", 'token' => 10001010, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 8,'message_id'=> 2, 'content' => "Content8", 'token' => 10001000, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 9,'message_id'=> 3, 'content' => "Content9", 'token' => 10001010, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 10,'message_id'=> 3, 'content' => "Content10", 'token' => 10001000, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 11,'message_id'=> 3, 'content' => "Content11", 'token' => 10001100, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 12,'message_id'=> 3, 'content' => "Content12", 'token' => 10011000, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 13,'message_id'=> 3, 'content' => "Content13", 'token' => 10101000, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")],
-            ['id' => 14,'message_id'=> 3, 'content' => "Content14", 'token' => 11001000, 'date' => date("Y-m-d H:i:s", "2000-05-13 12:25:33")]];
+        return [['id'=> 1,'message_id'=> 1, 'content' => "Content1", 'token' => 10001001, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 2,'message_id'=> 1, 'content' => "Content2", 'token' => 10001011, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 3,'message_id'=> 1, 'content' => "Content3", 'token' => 10001100, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 4,'message_id'=> 1, 'content' => "Content4", 'token' => 10001010, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 5,'message_id'=> 2, 'content' => "Content5", 'token' => 10001000, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 6,'message_id'=> 2, 'content' => "Content6", 'token' => 10101000, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 7,'message_id'=> 2, 'content' => "Content7", 'token' => 10001010, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 8,'message_id'=> 2, 'content' => "Content8", 'token' => 10001000, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 9,'message_id'=> 3, 'content' => "Content9", 'token' => 10001010, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 10,'message_id'=> 3, 'content' => "Content10", 'token' => 10001000, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 11,'message_id'=> 3, 'content' => "Content11", 'token' => 10001100, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 12,'message_id'=> 3, 'content' => "Content12", 'token' => 10011000, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 13,'message_id'=> 3, 'content' => "Content13", 'token' => 10101000, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))],
+            ['id' => 14,'message_id'=> 3, 'content' => "Content14", 'token' => 11001000, 'date' => date("Y-m-d H:i:s", mktime(3, 0, 0, 7, 1, 2000))]];
     }
+
 
     function generateRandomString($length = 10) {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -86,4 +87,70 @@ class PDOMessageModelTest extends TestCase {
         $this->assertEquals('array', gettype($actualMessage));
         $this->assertEquals($expectedMessage[0], $actualMessage[0]);
     }
+
+    public function testSearchByContent_messagesInDatabase(){
+        $messagesModel = new PDOMessageModel($this->connection);
+        $actualMessage = $messagesModel->searchByContent("content2");
+        $expectedMessage = $this->providerMessages();
+        $this->assertEquals('array', gettype($actualMessage));
+        $this->assertEquals($expectedMessage[1], $actualMessage[0]);
+    }
+
+    public function testSearchByContent_messagesNotInDatabase(){
+        $messagesModel = new PDOMessageModel($this->connection);
+        $actualMessage = $messagesModel->searchByContent("no content");
+        $this->assertEquals('array', gettype($actualMessage));
+        $this->assertEmpty($actualMessage);
+    }
+
+    public function testSearchByCategory_messagesInDatabase(){
+        $messagesModel = new PDOMessageModel($this->connection);
+        $actualMessage = $messagesModel->searchByCategory("Thriller");
+        $expectedMessage = $this->providerMessages();
+        $this->assertEquals('array', gettype($actualMessage));
+        $this->assertEquals($expectedMessage[2], $actualMessage[0]);
+    }
+
+    public function testSearchByCategory_messagesNotInDatabase(){
+        $messagesModel = new PDOMessageModel($this->connection);
+        $actualMessage = $messagesModel->searchByCategory("no category");
+        $this->assertEquals('array', gettype($actualMessage));
+        $this->assertEmpty($actualMessage);
+    }
+
+    public function testSearchByContentOrCategory_SearchByCategory_messagesInDatabase(){
+        $messagesModel = new PDOMessageModel($this->connection);
+        $actualMessage = $messagesModel->searchByContentAndCategory("content3","Thriller");
+        $expectedMessage = $this->providerMessages();
+        $this->assertEquals('array', gettype($actualMessage));
+        $this->assertEquals($expectedMessage[2], $actualMessage[0]);
+        $this->connection->getPDO()->prepare('SELECT * FROM messages WHERE id=?');
+    }
+
+    public function testSearchByContentOrCategory_SearchByCategory_messagesNotInDatabase(){
+        $messagesModel = new PDOMessageModel($this->connection);
+        $actualMessage = $messagesModel->searchByContentAndCategory("no content","no category");
+        $this->assertEquals('array', gettype($actualMessage));
+        $this->assertEmpty($actualMessage);
+    }
+
+    /**
+     * @dataProvider providerValidExistingIds
+     **/
+    public function testIdExists_existingId_True($id)
+    {
+        $messageModel = new PDOMessageModel($this->connection);
+        $this->assertTrue($messageModel->idExists($id));
+    }
+
+
+    /**
+     * @dataProvider providerValidUnexistingIds
+     **/
+    public function testIdExists_unexistingId_False($id)
+    {
+        $messageModel = new PDOMessageModel($this->connection);
+        $this->assertFalse($messageModel->idExists($id));
+    }
+
 }
