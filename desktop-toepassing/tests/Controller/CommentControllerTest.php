7f396d4665b40626b5b7904d8f956e252dada19e
PeetersGlenn <MrGlennPeeters@hotmail.com>
2018-11-08 17:36:30 +0100

    Extra testen in CategoryController en UserController

--- desktop-toepassing/tests/Controller/CommentControllerTest.php
+++ desktop-toepassing/tests/Controller/CommentControllerTest.php
@@ -2,12 +2,76 @@
 
 namespace App\Test\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CommentControllerTest extends WebTestCase
 {
-    public function testCommentController_200_comment(){
+    public function testPostCommentWithModel()
+    {
+        $commentId = uniqid();
+
+        $response = $this->client->post('/message/comment/post', [
+            'json' => [
+                'id'    => $commentId,
+                'content'     => 'Random message content',
+            ]
+        ]);
+
+        $this->assertEquals(201, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertEquals($commentId, $data['id']);
+    }
+
+    public function testPostCommentNoModel()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/message/comment/post');
+
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+    }
+
+
+    public function testUpdateCommentNoModel()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/message/comment/update');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testUpdateCommentWithModel()
+    {
+        $messageId = uniqid();
+
+        $response = $this->client->post('/message/comment/update', [
+            'json' => [
+                'id'    => $messageId,
+                'upVotes'     => 33,
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertEquals($messageId, $data['id']);
+    }
+
+    public function testDeleteComment()
+    {
+        $client = static::createClient();
+
+        $client->request('DELETE', '/message/comment/delete');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCommentController_GET_comment(){
         $client = static::createClient();
 
         $client->request('GET', '/comment');
@@ -16,7 +80,7 @@ class CommentControllerTest extends WebTestCase
 
     }
 
-    public function testCommentController_200_commentUpdate(){
+    public function testCommentController_GET_commentUpdate(){
         $client = static::createClient();
 
         $client->request('GET', '/comment/update');
@@ -25,7 +89,7 @@ class CommentControllerTest extends WebTestCase
 
     }
 
-    public function testCommentController_200_commentDelete(){
+    public function testCommentController_GET_commentDelete(){
         $client = static::createClient();
 
         $client->request('GET', '/comment/delete');
